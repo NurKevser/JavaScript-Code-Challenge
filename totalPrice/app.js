a95d4aa9f98dab31f7adb705a38240a07cb3f3d1
NurKevser <nurkevserkadiroglu@gmail.com>
2021-07-05 22:09:39 +0300

    total price

--- totalPrice/app.js
+++ totalPrice/app.js
@@ -5,5 +5,13 @@ function totalPrice(price,tax) {
 console.log(totalPrice(50,18));
 
 
-
+function totalPrice(tax, ...args) {
+    sum = 0
+    for(i = 0; i < args.length; i++){
+        a = args[i] + (args[i]*18) / 100;
+        sum += a;
+    }
+    return sum
+}
+console.log(totalPrice(18,20,10,30))
 
