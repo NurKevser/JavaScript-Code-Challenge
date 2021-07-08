88ffcd7134078fdc891c4f347bdfba3678d5dcb7
NurKevser <nurkevserkadiroglu@gmail.com>
2021-07-08 12:43:07 +0300

    reversed string

--- reverse.js
+++ reverse.js
@@ -4,3 +4,10 @@ for(let i = text.length; i> 0; i--){
     newText += text[i-1]
 }
 document.write(newText); // console a değil ekrana yazdırır.
+
+
+//     SOLUTION 2
+function rev_str(s){
+    return s.split("").reverse().join("");
+}
+console.log(rev_str("clarusway"));
