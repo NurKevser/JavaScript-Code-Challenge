501973d83c44b4d7460dbe40cdb6a62ab342f048
NurKevser <nurkevserkadiroglu@gmail.com>
2021-08-14 13:55:48 +0300

    created new folder

---
+++ inclass_challange/app.js
@@ -0,0 +1,63 @@
+const movements = [100, 300, -200, 500, -400, 225, 1000, -700];
+// 1
+// You deposited $xx
+// You withdrew $xxx 
+
+//  arrDeposits =
+//  arrWithdraws =
+// for each
+//
+
+
+
+
+
+
+
+
+
+/******** */
+
+// map initials
+const str = 'Clarusway Online Career IT Training School';
+
+
+
+
+
+
+
+
+
+/***** */
+// map
+
+const str = 'The Quick Brown Fox'; // tHe YeLLoW fOx 
+// Write a JavaScript program which accept a string as input and swap the case of each character. For example if you input 'The Quick Brown Fox' the output should be 'tHE qUICK bROWN fOX'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// filter words longer than 6 letters
+const words = [
+  'spray',
+  'limit',
+  'elite',
+  'exuberant',
+  'destruction',
+  'present',
+  'Clarusway',
+  'Future',
+];
