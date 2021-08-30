e36d3fec66f3511758d90a7ca98089957476b306
NurKevser <nurkevserkadiroglu@gmail.com>
2021-08-30 23:52:31 +0300

    get first letter of sentence in a string with map

--- inclass_challange/app.js
+++ inclass_challange/app.js
@@ -20,6 +20,8 @@ const movements = [100, 300, -200, 500, -400, 225, 1000, -700];
 
 // map initials
 const str = 'Clarusway Online Career IT Training School';
+console.log(str.split(" ").map(x=> x[0])) ;
+
 
 
 
@@ -32,7 +34,7 @@ const str = 'Clarusway Online Career IT Training School';
 /***** */
 // map
 
-const str = 'The Quick Brown Fox'; // tHe YeLLoW fOx 
+const str1 = 'The Quick Brown Fox'; // tHe YeLLoW fOx 
 // Write a JavaScript program which accept a string as input and swap the case of each character. For example if you input 'The Quick Brown Fox' the output should be 'tHE qUICK bROWN fOX'
 
 
